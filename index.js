e2634eda29081a2b4dcc399f0003d499bc2f2356
rajendrachoudhary2028@gmail.com <rajendrachoudhary2028@gmail.com>
2021-10-01 21:47:16 +0530

    edited

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ let globalTaskData = [];
 const generateHTML = (taskData) => `<div id=${taskData.id} class="col-lg-4 col-md-6 my-4">
 <div class="card">
     <div class="card-header d-flex gap-2 justify-content-end">
-        <button class="btn btn-outline-info">
-            <i class="fal fa-pencil"></i>
+        <button class="btn btn-outline-info" name=${taskData.id} onclick="editCared.apply(this, arguments)">
+            <i class="fal fa-pencil" name=${taskData.id}></i>
         </button>
         <button class="btn btn-outline-danger" name=${taskData.id} onclick="deleteCard.apply(this, arguments)">
             <i class="far fa-trash-alt" name=${taskData.id}></i>
@@ -100,5 +100,58 @@ const deleteCard = (event) => {
             event.target.parentNode.parentNode.parentNode.parentNode
         );
     }
+};
+
+const editCared = (event) => {
+    const elementType = event.target.tagName;
+
+    let taskTitle;
+    let taskType;
+    let taskDescription;
+    let parentElement;
+    let submitButton;
+
+    if (elementType === "BUTTON") {
+        parentElement = event.target.parentNode.parentNode
+    }else{
+        parentElement = event.target.parentNode.parentNode.parentNode
+    }
+
+    taskTitle = parentElement.childNodes[3].childNodes[3];
+    taskDescription = parentElement.childNodes[3].childNodes[5];
+    taskType = parentElement.childNodes[3].childNodes[7];
+    submitButton = parentElement.childNodes[5].childNodes[1];
+
+    taskTitle.setAttribute("contenteditable", "true");
+    taskDescription.setAttribute("contenteditable", "true");
+    taskType.setAttribute("contenteditable", "true");
+    submitButton.innerHTML = "Save Changes";
+};
+
+const saveEdit = (event) => {
+    const targetId = event.target.getAttribute("name");
+    const elementType = event.target.tagName;
+    
+    const taskTitle = parentElement.childNodes[3].childNodes[3];
+    const taskDescription = parentElement.childNodes[3].childNodes[5];
+    const taskType = parentElement.childNodes[3].childNodes[7];
+    const submitButton = parentElement.childNodes[5].childNodes[1];
+
+    const updateData = {
+        title: taskTitle,
+        type: taskType,
+        description: taskDescription
+    };
+    
+    globalTaskData.forEach((task) => {
+        if (task.id === targetId) {
+            return {...task, ...updateData};
+        }
+        return task;
+    });
+
+    saveToLocalStorage();
+
+    
 };
 
